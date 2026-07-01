228244ad3bdc0dc261d4c74d4024196c03471632
vsgames42 <vsgames42@gmail.com>
2026-07-01 19:20:46 +0530

    Export cron job for health check requests to allow external usage

--- backend/src/lib/cron.js
+++ backend/src/lib/cron.js
@@ -3,7 +3,7 @@ import http from "node:http";
 import https from "node:https";
 
 // every 14 minutes send a GET request to the health endpoint
-const job = new CronJob("*/14 * * * *", function () {
+export const job = new CronJob("*/14 * * * *", function () {
   const base = process.env.FRONTEND_URL;
   if (!base) return;
   const url = new URL("/health", base).href;
@@ -16,5 +16,3 @@ const job = new CronJob("*/14 * * * *", function () {
     })
     .on("error", (e) => console.error("Error while sending request", e));
 });
-
-export default job;
